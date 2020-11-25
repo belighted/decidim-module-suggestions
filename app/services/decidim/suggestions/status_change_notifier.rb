309fba708cd64eccad53d84cfd2cfbf714f29a67
sergei-krylov <sergei.krylov@yahoo.com>
2020-11-25 13:13:18 +0300

    BOSA-191 Fix status change notifications

--- app/services/decidim/suggestions/status_change_notifier.rb
+++ app/services/decidim/suggestions/status_change_notifier.rb
@@ -46,30 +46,37 @@ module Decidim
       end
 
       def notify_validating_result
+        notify_committee_members
+        notify_author
+      end
+
+      def notify_support_result
+        notify_followers
+        notify_committee_members
+        notify_author
+      end
+
+      def notify_committee_members
         suggestion.committee_members.approved.each do |committee_member|
+          next if suggestion.author == committee_member.user
+
           Decidim::Suggestions::SuggestionsMailer
             .notify_state_change(suggestion, committee_member.user)
             .deliver_later
         end
-
-        Decidim::Suggestions::SuggestionsMailer
-          .notify_state_change(suggestion, suggestion.author)
-          .deliver_later
       end
 
-      def notify_support_result
+      def notify_followers
         suggestion.followers.each do |follower|
+          next if suggestion.author == follower
+
           Decidim::Suggestions::SuggestionsMailer
             .notify_state_change(suggestion, follower)
             .deliver_later
         end
+      end
 
-        suggestion.committee_members.approved.each do |committee_member|
-          Decidim::Suggestions::SuggestionsMailer
-            .notify_state_change(suggestion, committee_member.user)
-            .deliver_later
-        end
-
+      def notify_author
         Decidim::Suggestions::SuggestionsMailer
           .notify_state_change(suggestion, suggestion.author)
           .deliver_later
